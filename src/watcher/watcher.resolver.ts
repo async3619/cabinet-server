63540ec4ef93c5e96bf32a7e5684dd7c5051b3ec
Sophia (Turner) <me@sophia-dev.io>
2025-05-05 02:18:01 +0900

    feat(watcher): improve watch graphql resolver

--- src/watcher/watcher.resolver.ts
+++ src/watcher/watcher.resolver.ts
@@ -1,12 +1,15 @@
 import { Inject } from '@nestjs/common'
-import { Resolver, Query, Args, ResolveField, Root } from '@nestjs/graphql'
+import { Resolver, Query, Args, ResolveField, Root, Int } from '@nestjs/graphql'
 
 import {
   Attachment,
   FindFirstWatcherArgs,
+  FindManyAttachmentArgs,
+  FindManyThreadArgs,
   FindManyWatcherArgs,
   Thread,
   Watcher,
+  WatcherCount,
 } from '@/generated/graphql'
 import { WatcherService } from '@/watcher/watcher.service'
 
@@ -15,6 +18,11 @@ export class WatcherResolver {
   constructor(
     @Inject(WatcherService) private readonly watcherService: WatcherService,
   ) {}
+
+  @Query(() => Int)
+  async watcherCount(): Promise<number> {
+    return this.watcherService.count()
+  }
 
   @Query(() => Watcher, { nullable: true })
   async watcher(@Args() args: FindFirstWatcherArgs): Promise<Watcher | null> {
@@ -27,20 +35,37 @@ export class WatcherResolver {
   }
 
   @ResolveField(() => [Thread])
-  async threads(@Root() watcher: Watcher) {
+  async threads(@Root() watcher: Watcher, @Args() args: FindManyThreadArgs) {
     return this.watcherService
       .findOne({
         where: { id: watcher.id },
       })
-      .threads()
+      .threads(args)
   }
 
   @ResolveField(() => [Attachment])
-  async attachments(@Root() watcher: Watcher) {
+  async attachments(
+    @Root() watcher: Watcher,
+    @Args() args: FindManyAttachmentArgs,
+  ) {
     return this.watcherService
       .findOne({
         where: { id: watcher.id },
       })
-      .attachments()
+      .attachments(args)
+  }
+
+  @ResolveField(() => WatcherCount)
+  async _count(@Root() watcher: Watcher) {
+    const result = await this.watcherService.findOne({
+      select: { _count: true },
+      where: { id: watcher.id },
+    })
+
+    if (!result) {
+      throw new Error('Relation count aggregation failed')
+    }
+
+    return result._count
   }
 }
