abf503bff6ddee7e9863adeab5ee01e160dc5cd4
Sophia (Turner) <me@sophia-dev.io>
2025-05-05 01:26:38 +0900

    feat(thread): implement thread resolver

--- src/thread/thread.module.ts
+++ src/thread/thread.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 
 import { AttachmentModule } from '@/attachment/attachment.module'
+import { ThreadResolver } from '@/thread/thread.resolver'
 import { ThreadService } from '@/thread/thread.service'
 
 @Module({
   imports: [AttachmentModule],
-  providers: [ThreadService],
+  providers: [ThreadService, ThreadResolver],
   exports: [ThreadService],
 })
 export class ThreadModule {}
