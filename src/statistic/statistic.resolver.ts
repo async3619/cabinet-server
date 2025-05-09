d7d1911a13fe1baada090fe9b925bb14314dbcc8
Sophia (Turner) <me@sophia-dev.io>
2025-05-10 03:10:19 +0900

    fix(statistics): make `totalSize` field of `statistic` entity to return `BigInt` (#48)

--- src/statistic/statistic.resolver.ts
+++ src/statistic/statistic.resolver.ts
@@ -1,5 +1,6 @@
 import { Inject } from '@nestjs/common'
-import { Query, Resolver, Args } from '@nestjs/graphql'
+import { Query, Resolver, Args, ResolveField, Root } from '@nestjs/graphql'
+import { GraphQLBigInt } from 'graphql-scalars'
 
 import { FindManyStatisticArgs, Statistic } from '@/generated/graphql'
 import { StatisticService } from '@/statistic/statistic.service'
@@ -14,5 +15,10 @@ export class StatisticResolver {
   @Query(() => [Statistic])
   async statistics(@Args() args: FindManyStatisticArgs) {
     return this.statisticService.find(args)
+  }
+
+  @ResolveField(() => GraphQLBigInt)
+  async totalSize(@Root() statistic: Statistic) {
+    return statistic.totalSize
   }
 }
