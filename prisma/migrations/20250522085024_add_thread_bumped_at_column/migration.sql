db27980353d12292b0dfd1184fadb33dff693107
Sophia (Turner) <me@sophia-dev.io>
2025-05-22 17:58:13 +0900

    chore(database): add `bumpedAt` column to thread table

---
+++ prisma/migrations/20250522085024_add_thread_bumped_at_column/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Thread" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "no" INTEGER NOT NULL,
+    "author" TEXT NOT NULL,
+    "title" TEXT,
+    "content" TEXT,
+    "createdAt" DATETIME NOT NULL,
+    "bumpedAt" DATETIME NOT NULL DEFAULT '1970-01-01',
+    "boardId" TEXT NOT NULL,
+    CONSTRAINT "Thread_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Thread" ("author", "boardId", "content", "createdAt", "id", "no", "title") SELECT "author", "boardId", "content", "createdAt", "id", "no", "title" FROM "Thread";
+DROP TABLE "Thread";
+ALTER TABLE "new_Thread" RENAME TO "Thread";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
