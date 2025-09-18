be352b977fd32388a7998c1070cee89d54cd50d1
Sophia (Turner) <me@sophia-dev.io>
2025-09-18 22:13:11 +0900

    feat(config): support YAML configuration files and enhance loading logic

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -4,9 +4,10 @@ import {
   OnModuleDestroy,
   OnModuleInit,
 } from '@nestjs/common'
-import { formatJsonError } from 'better-zod-errors'
+import { formatJsonError, formatYamlError } from 'better-zod-errors'
 import * as chokidar from 'chokidar'
 import * as fs from 'fs-extra'
+import * as yaml from 'js-yaml'
 import { z } from 'zod'
 
 import * as path from 'node:path'
@@ -39,7 +40,12 @@ const configDataSchema = z
  */
 export type ConfigData = z.infer<typeof configDataSchema>
 
-const CONFIG_FILE_PATH = path.join(process.cwd(), 'cabinet.config.json')
+const AVAILABLE_CONFIG_FILE_PATHS = [
+  path.join(process.cwd(), 'cabinet.config.json'),
+  path.join(process.cwd(), 'cabinet.config.yml'),
+  path.join(process.cwd(), 'cabinet.config.yaml'),
+]
+
 const CONFIG_SCHEMA_FILE_PATH = path.join(
   process.cwd(),
   'cabinet.config.schema.json',
@@ -96,10 +102,13 @@ export class ConfigService
       }
     }
 
-    await this.loadConfig()
+    const configFilePath = await this.loadConfig()
 
-    this.watcher = chokidar.watch(CONFIG_FILE_PATH)
-    this.watcher.on('change', this.handleConfigChange.bind(this))
+    this.watcher = chokidar.watch(configFilePath)
+    this.watcher.on(
+      'change',
+      this.handleConfigChange.bind(this, configFilePath),
+    )
   }
 
   async onModuleDestroy() {
@@ -111,19 +120,43 @@ export class ConfigService
     this.watcher = null
   }
 
-  private async loadConfig() {
-    if (!fs.existsSync(CONFIG_FILE_PATH)) {
-      throw new Error('Config file not found!')
+  private searchForConfigFile() {
+    const existingConfigFilePaths = AVAILABLE_CONFIG_FILE_PATHS.filter((p) =>
+      fs.existsSync(p),
+    )
+
+    if (existingConfigFilePaths.length === 0) {
+      throw new Error(
+        'There was no config file found. Tried: \n' +
+          AVAILABLE_CONFIG_FILE_PATHS.map((p) => `  - ${p}`).join('\n'),
+      )
     }
 
-    const config = await fs.readJson(CONFIG_FILE_PATH)
+    if (existingConfigFilePaths.length > 1) {
+      this.logger.warn(
+        'Multiple config files found, using the first one: ' +
+          existingConfigFilePaths[0],
+      )
+    }
+
+    return existingConfigFilePaths[0]
+  }
+
+  private async loadConfig(targeFilePath?: string) {
+    const configFilePath = targeFilePath ?? this.searchForConfigFile()
+    const isJson = path.extname(configFilePath).toLowerCase() === '.json'
+
+    const config = isJson
+      ? await fs.readJson(configFilePath)
+      : yaml.load(await fs.promises.readFile(configFilePath, 'utf-8'))
 
     try {
       this.currentConfig = configDataSchema.parse(config)
     } catch (error) {
       if (error instanceof z.ZodError) {
+        const formatter = isJson ? formatJsonError : formatYamlError
         const formattedErrors = error.issues.map((issue) =>
-          formatJsonError(issue, config, {
+          formatter(issue, config, {
             useColor: true,
             syntaxHighlighting: true,
           }),
@@ -134,13 +167,15 @@ export class ConfigService
         throw error
       }
     }
+
+    return configFilePath
   }
 
-  private async handleConfigChange() {
+  private async handleConfigChange(targetFilePath: string) {
     this.logger.warn('Server configuration changed, reloading configuration...')
 
     try {
-      await this.loadConfig()
+      await this.loadConfig(targetFilePath)
       this.logger.log('Reloaded server configuration file successfully.')
       this.emit('change')
     } catch (e) {
